2aca101fc4ba1184d00bc4b5ef648eba9cd9c1c6
hloydl <harald.loydl@loydl.ch>
2017-03-15 21:20:08 +0100

    feat(GraphQL Task): added complex field 'processInstance'

--- src/main/java/graphql/resolvers/TaskEntityResolver.java
+++ src/main/java/graphql/resolvers/TaskEntityResolver.java
@@ -5,9 +5,11 @@ import org.camunda.bpm.BpmPlatform;
 import org.camunda.bpm.engine.ProcessEngine;
 import org.camunda.bpm.engine.ProcessEngines;
 import org.camunda.bpm.engine.RepositoryService;
+import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.impl.persistence.entity.ExecutionEntity;
 import org.camunda.bpm.engine.impl.persistence.entity.TaskEntity;
 import org.camunda.bpm.engine.repository.ProcessDefinition;
+import org.camunda.bpm.engine.runtime.ProcessInstance;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -15,6 +17,7 @@ public class TaskEntityResolver extends GraphQLResolver {
 
     private ProcessEngine pe;
     private RepositoryService repositoryService;
+    private RuntimeService runtimeService;
 
     public TaskEntityResolver() {
         super(TaskEntity.class);
@@ -23,6 +26,7 @@ public class TaskEntityResolver extends GraphQLResolver {
             pe = ProcessEngines.getDefaultProcessEngine(false);
         }
         repositoryService = pe.getRepositoryService();
+        runtimeService = pe.getRuntimeService();
     }
 
     public ProcessDefinition processDefinition(TaskEntity taskEntity) {
@@ -30,6 +34,15 @@ public class TaskEntityResolver extends GraphQLResolver {
         if (pdid != null) {
             ProcessDefinition processDefinition = repositoryService.getProcessDefinition(pdid);
             return processDefinition;
+        } else
+            return null;
+    }
+
+    public ProcessInstance processInstance (TaskEntity taskEntity) {
+        String piId = taskEntity.getProcessInstanceId();
+        if (piId != null) {
+            ProcessInstance pi = runtimeService.createProcessInstanceQuery().processInstanceId(piId).singleResult();
+            return pi;
         } else
             return null;
     }
