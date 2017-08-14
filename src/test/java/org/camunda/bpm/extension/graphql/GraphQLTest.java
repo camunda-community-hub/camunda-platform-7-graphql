4eab97c8f3ae60c3f8359ab1fca910a4af7b3bef
hloydl <harald.loydl@loydl.ch>
2017-08-14 20:04:47 +0200

    test(Query): add test for processInstances

--- src/test/java/org/camunda/bpm/extension/graphql/GraphQLTest.java
+++ src/test/java/org/camunda/bpm/extension/graphql/GraphQLTest.java
@@ -2,6 +2,10 @@ package org.camunda.bpm.extension.graphql;
 
 import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
 
+import graphql.ExecutionResult;
+import graphql.GraphQL;
+import graphql.execution.ExecutionStrategy;
+import graphql.schema.GraphQLSchema;
 import org.camunda.bpm.engine.RuntimeService;
 import org.camunda.bpm.engine.repository.ProcessDefinition;
 import org.camunda.bpm.engine.runtime.ProcessInstance;
@@ -10,7 +14,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.HashMap;
@@ -19,8 +22,10 @@ import java.util.Map;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.when;
 
+import static org.camunda.spin.Spin.JSON;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {TestConfig.class, EngineConfig.class})
+@SpringBootTest(classes = {GraphQLServer.class, TestConfig.class, EngineConfig.class})
 public class GraphQLTest {
 
     private static final String PROCESS_KEY = "credit-application";
@@ -33,6 +38,12 @@ public class GraphQLTest {
     @Autowired
     private CustomerDataService customerDataServiceMock;
 
+    @Autowired
+    private GraphQLSchema graphQLSchema;
+
+    @Autowired
+    private ExecutionStrategy executionStrategy;
+
     @Test
     public void processDeployed() throws Exception {
         ProcessDefinition definition = processDefinition(PROCESS_KEY);
@@ -40,16 +51,29 @@ public class GraphQLTest {
     }
 
     @Test
-    public void queryTask() throws Exception {
+    public void queryProcessInstance() throws Exception {
+        final String query = "{ processInstances { id } }";
+        //final String expectedResult = "{\"processInstances\":[{\"id\":\"1\"}]}";
+
         CustomerData customer = new CustomerData(EXAMPLE_ID, "Dummy Corp.", Personality.JURIDICAL, SolvencyRating.A);
         when(customerDataServiceMock.findById(EXAMPLE_ID)).thenReturn(customer);
 
         ProcessInstance processInstance = runtimeService.startProcessInstanceByKey(PROCESS_KEY, BUSINESS_KEY, startFormEntries(450000));
         ProcessEngineAssertions.assertThat(processInstance).isNotEnded();
 
-        // query GraphQL endpoint...
-        //GraphQL graphQL = new GraphQL(graphQLSchema,executionStrategy);
-        //ExecutionResult executionResult = graphQL.execute("{ processesInstances {    id } }");
+        GraphQL graphQL = new GraphQL(graphQLSchema,executionStrategy);
+        ExecutionResult executionResult = graphQL.execute(query);
+        HashMap<String,Object> map = (HashMap<String,Object>)(executionResult.getData());
+
+        // Expect No Errors
+        assertThat(executionResult.getErrors().size()).isEqualTo(0);
+
+        // Expect exactly one process instance
+        assertThat(map.size()).isEqualTo(1);
+        assertThat(map.keySet().toArray()[0]).isEqualTo("processInstances");
+
+        String actualResult = JSON(map).toString();
+        //@todo: use a JSON Validator? check out http://json-schema.org/implementations.html
 
     }
 
