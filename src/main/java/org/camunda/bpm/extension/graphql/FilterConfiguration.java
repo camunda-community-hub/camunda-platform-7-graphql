8554608a1872eb361d5895e28e4a75b18ea0b5de
herr-vogel <herr.d.vogel@gmail.com>
2017-09-27 09:03:34 +0200

    refactor(FilterConfiguration): remove CORS filter (provided in grpahql-spring-boot-starter)

--- src/main/java/org/camunda/bpm/extension/graphql/FilterConfiguration.java
+++ src/main/java/org/camunda/bpm/extension/graphql/FilterConfiguration.java
@@ -47,18 +47,4 @@ public class FilterConfiguration {
 		return registration;
 	}
 
-	@Bean
-	public FilterRegistrationBean corsFilter() {
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("*");
-		source.registerCorsConfiguration("/**", config);
-		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
-		bean.setOrder(0);
-		return bean;
-	}
-
 }
